4601fe7a7c20224c19826b2c8492ad0bc772033a
quessissues <vlasenko.danil111@gmail.com>
2024-05-10 02:05:59 +0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,21 +3,5 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title('Danilc apps')
+st.title('Визуализация данных')
 
-st.text("Hello, this is my app.")
-
-st.write("This is a Streamlit app used to demonstrate interactive widgets and plots!")
-
-user_input = st.text_input("Please enter your name")
-
-if user_input:
-    st.write(f"Hello, {user_input}! Welcome to the Streamlit app.")
-
-number = st.slider("Select a number", 0, 100, 25)
-st.write(f"You selected: {number}")
-
-data = pd.DataFrame(
-    np.random.randn(50, 2), 
-    columns=['A', 'B']
-)
